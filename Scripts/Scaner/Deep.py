6755e10f59a7974dbeb637683dce6d89c8f365b6
MekoNix <64309709+MekoNix@users.noreply.github.com>
2024-01-14 22:10:28 +0300

    Scaner 0.0.1 Test

---
+++ Scripts/Scaner/Deep.py
@@ -0,0 +1,6 @@
+from pymetasploit3.msfrpc import MsfRpcClient
+
+
+client = MsfRpcClient('Root', server='127.0.0.1')
+
+
---
+++ Scripts/Scaner/Light.py
@@ -0,0 +1,41 @@
+from Scripts.log import *
+from Scripts.modules import *
+from pymongo import MongoClient
+
+
+def credentials_check(host):
+    host = pars_json("Host", host)
+    port = int(pars_json("Port", host))
+    username = pars_json("User", host)
+    password = pars_json("Password", host)
+    authSource = pars_json("authSource", host)
+    try:
+        MongoClient(host=host, port=port, username=username, password=password, authSource=authSource)
+    except Exception as e:
+        log_event("Неверный пароль или пользователь. Пожалуйста, проверьте правильность введенных данных и повторите попытку.",'Warning',npt=1)
+
+
+def db_ver(host):
+
+    host = pars_json("Host",host)
+    port = int(pars_json("Port",host))
+    username=pars_json("User",host)
+    password=pars_json("Password",host)
+    authSource=pars_json("authSource",host)
+
+    mongo_client = MongoClient(host=host, port=port,username=username,password=password,authSource=authSource)
+
+    server_info = mongo_client.server_info()
+
+    print(f"Версия MongoDB: {server_info['version']}")
+    return server_info['version']
+
+def found_CVE_by_ver(ver):
+    found=[]
+    cve=find_path("../Server/app/static/cve.json")
+    with open(cve, 'r') as file:
+        cve_data = json.load(file)
+    for cve, versions in cve_data.items():
+        if ver in versions:
+            found.append(cve)
+    return found
